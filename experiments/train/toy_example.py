83e265ab8ae638e9d2ea49d5e1894d431b8fda75
Jakob <jakob.lindqvist@chalmers.se>
2019-03-19 13:50:41 +0100

    Moved logic to model class

--- experiments/train/toy_example.py
+++ experiments/train/toy_example.py
@@ -11,7 +11,9 @@ import torch
 import torch.nn.functional as F
 import torchvision
 
-from swag import data, models, utils, losses
+# from swag import data, models, utils, losses
+from swag import data
+from swag import models
 from swag.posteriors import SWAG
 
 
@@ -19,28 +21,17 @@ def main():
     """Main entry point"""
     mean = np.array([2, 2])
     cov = np.diag([1, 1])
-    dataset = data.SyntheticGaussianData(mean=mean, cov=cov, n_samples=100)
+    dataset = data.SyntheticGaussianData(mean=mean, cov=cov, n_samples=1000)
     data_train_loader = torch.utils.data.DataLoader(dataset,
-                                                    batch_size=1,
+                                                    batch_size=5,
                                                     shuffle=True)
     model = models.gaussian_likelihood.GaussianLikelihood(dim=2)
-    criterion = torch.nn.L1Loss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                                step_size=10,
-                                                gamma=0.2)
 
-    const_zero = torch.zeros(1, dtype=torch.double)
-    num_epochs = 50
+    num_epochs = 100
     for epoch in range(num_epochs):
-        print("Epoch: {}\t Mean: {}".format(epoch,
-                                            model.mean.data.cpu().numpy()))
-        for sample in data_train_loader:
-            loss = criterion(model(sample), const_zero)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        scheduler.step()
+        print("Epoch: {}\t {}".format(epoch, model.status()))
+        model.train_epoch(data_train_loader)
+        model.update_learning_rate()
 
 
 if __name__ == "__main__":
--- swag/models/gaussian_likelihood.py
+++ swag/models/gaussian_likelihood.py
@@ -7,17 +7,66 @@ from torch.distributions.multivariate_normal import MultivariateNormal
 
 
 class GaussianLikelihood(nn.Module):
+    """Gaussian likelihood model
+
+    Quasi model with no response variable
+    Finds parameters such that a given dataset is approximated
+    by a multivariate gaussian.
+    """
 
     def __init__(self, dim):
         super(GaussianLikelihood, self).__init__()
-        self.mean = torch.nn.Parameter(torch.zeros(dim, dtype=torch.double),
-                                       requires_grad=True)
-        self.cov = torch.tensor(np.eye(dim))
+
+        self.mean = torch.nn.Parameter(torch.zeros(dim, dtype=torch.double))
+        self.cov = torch.tensor(np.eye(dim), dtype=torch.double)
+
+        self.params = nn.ParameterDict({"mean": self.mean})
         self.dist = MultivariateNormal(self.mean, self.cov)
+        self.criterion = torch.nn.L1Loss()
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=1e-1)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,
+                                                         step_size=20,
+                                                         gamma=0.2)
 
+    def status(self, include_learning_rate=True):
+        """Format parameter values into a tab separated string"""
+        status = str()
+        for key, param in self.params.items():
+            val = param.data.cpu().numpy()
+            status += "{}: {}\t".format(key, val)
+        if include_learning_rate:
+            status += "Learning rate: {}\t" .format(
+                self.optimizer.param_groups[0]["lr"])
+        return status
 
-    def forward(self, x):
-        return - self.dist.log_prob(x)
+    def train_epoch(self, data_train_loader):
+        """Train epoch"""
+        for sample in data_train_loader:
+            loss = self.neg_log_likelihood(sample)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+    def neg_log_likelihood(self, sample):
+        """Custom loss
+
+        Calc neg log lik by a forward pass
+        Measure L1 norm from zero (conforming to pytorch)
+        There is probably a better way.
+        """
+
+        neg_log_likelihood = self(sample)
+        return self.criterion(neg_log_likelihood,
+                              torch.zeros(1, dtype=torch.double))
+
+    def update_learning_rate(self, epoch=None):
+        """Update learning rate"""
+        self.scheduler.step(epoch)
+
+    def forward(self, sample):
+        """Forward pass"""
+        return - self.dist.log_prob(sample)
 
     def true_posterior(self, cov_data):
+        """Calculate analytic posterior"""
         raise NotImplementedError
